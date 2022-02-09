3f1edb24241704ea955f20919c80a5ce8372fdd9
LauraConnolly <15lpc1@queensu.ca>
2022-02-09 14:45:11 -0500

    Added logic for translating and rotating/ then applying FK. Doesn't seem to be doing correct placement - note: origin and rpy hard coded for now.

--- Logic/vtkSlicerSlicerRos2Logic.cxx
+++ Logic/vtkSlicerSlicerRos2Logic.cxx
@@ -119,115 +119,119 @@ void vtkSlicerSlicerRos2Logic
     return; //std::cerr << "No urdf file to load." << filename << std::endl;
   }
 
-
-  // Solve the forward kinematics of the KDL tree
-  KDL::Chain kdl_chain;
-  std::string base_frame("upper_arm"); // Specify the base to tip you want ie. joint 1 to 2 (base to torso)
-  std::string tip_frame("lower_arm");
-  if (!my_tree.getChain(base_frame, tip_frame, kdl_chain))
-  {
-    std::cerr << "not working" << std::endl;
-    return;
-  }
-
-  // TODO: replace the hard coded link names with something like this
-  KDL::Joint kdl_joint = kdl_chain.getSegment(0).getJoint();
-  KDL::Segment kdl_segment = kdl_chain.getSegment(0);
-  std::string segmentName(kdl_segment.getName());
-  std::string jointName(kdl_joint.getName());
-  KDL::Vector vect = kdl_joint.JointOrigin();
-  KDL::Vector rot = kdl_joint.JointAxis(); // this doesn't seem to be the RPY (figure out how to get that)
-  std::cout << vect.operator()(0) <<std::endl;
-  std::cout << rot.operator()(0) <<std::endl;
-  std::cerr << jointName << std::endl; // this gives you the joint names (lower_arm, upper_arm) - need to figure out how to get base, torso
-  std::cerr << segmentName << std::endl;
-
-  ChainFkSolverPos_recursive fksolver = ChainFkSolverPos_recursive(kdl_chain);
-
-  // Create joint array
-  unsigned int nj = kdl_chain.getNrOfJoints();
-  KDL::JntArray jointpositions = JntArray(nj);
-
-  // Create the frame that will contain the results
-  KDL::Frame cartpos;
-  float x = 0;
-
-  // Calculate forward position kinematics
-  bool kinematics_status;
-  kinematics_status = fksolver.JntToCart(jointpositions,cartpos);
-  if(kinematics_status>=0){
-      std::cout << cartpos <<std::endl;
-      x = cartpos.operator()(2,3);
-      std::cout << cartpos.operator()(2,3) <<std::endl;
-      std::cout << "Thanks KDL!" <<std::endl;
-  }else{
-      printf("%s \n","Error: could not calculate forward kinematics :(");
-  }
-
-  // Create a transform node to add the model to so we can move it around
-  vtkNew<vtkMRMLTransformStorageNode> storageNode;
-  vtkSmartPointer<vtkMRMLTransformNode> tnode;
-
-	storageNode->SetScene(this->GetMRMLScene());
-
- 	vtkNew<vtkMRMLTransformNode> generalTransform;
- 	generalTransform->SetScene(this->GetMRMLScene());
-  tnode = vtkSmartPointer<vtkMRMLTransformNode>::Take(vtkMRMLLinearTransformNode::New());
- 	storageNode->ReadData(tnode.GetPointer());
-  tnode->SetName("ForwardKinematics");
-  this->GetMRMLScene()->AddNode(storageNode.GetPointer());
- 	this->GetMRMLScene()->AddNode(tnode);
-  tnode->SetAndObserveStorageNodeID(storageNode->GetID());
-
-  // Create a second transform for position (translation and rotation)
-
-  vtkNew<vtkMRMLTransformStorageNode> positionStorageNode;
-  vtkSmartPointer<vtkMRMLTransformNode> ptnode;
-
-	positionStorageNode->SetScene(this->GetMRMLScene());
-  vtkNew<vtkMRMLTransformNode> positionTransform;
- 	positionTransform->SetScene(this->GetMRMLScene());
-  ptnode = vtkSmartPointer<vtkMRMLTransformNode>::Take(vtkMRMLLinearTransformNode::New());
- 	positionStorageNode->ReadData(ptnode.GetPointer());
-  ptnode->SetName("InitialPosition");
-  this->GetMRMLScene()->AddNode(positionStorageNode.GetPointer());
- 	this->GetMRMLScene()->AddNode(ptnode);
-  ptnode->SetAndObserveStorageNodeID(positionStorageNode->GetID());
-
-  vtkTransform *modifiedTransform = vtkTransform::SafeDownCast(ptnode->GetTransformToParent());
-  modifiedTransform->Translate(0, 0, 0);
-  modifiedTransform->RotateX(90);
-  ptnode->SetAndObserveTransformToParent(modifiedTransform);
-  ptnode->Modified();
-
-  //Stack the transforms
-  tnode->SetAndObserveTransformNodeID(ptnode->GetID());
-
-
   //Initialize python so you can call load modules
-  QList<QVariant> cartesian_pos;
-  cartesian_pos.append(x);
   #ifdef Slicer_USE_PYTHONQT
     PythonQt::init();
     PythonQtObjectPtr context = PythonQt::self()->getMainModule();
-    context.addVariable("cartpos", cartesian_pos);
     context.evalScript(QString(
     "import slicer \n"
+    "slicer.util.loadModel(r'/home/laura/ros2_ws/src/SlicerRos2/models/meshes/base.stl') \n"
+    "slicer.util.loadModel(r'/home/laura/ros2_ws/src/SlicerRos2/models/meshes/torso.stl') \n"
     "slicer.util.loadModel(r'/home/laura/ros2_ws/src/SlicerRos2/models/meshes/upper_arm.stl') \n"
     "slicer.util.loadModel(r'/home/laura/ros2_ws/src/SlicerRos2/models/meshes/lower_arm.stl') \n"));
   #endif
 
-  // Add model for the joint to transform hiearchy - so it moves with the transform
-  vtkMRMLModelNode *modelNode = vtkMRMLModelNode::SafeDownCast(this->GetMRMLScene()->GetFirstNodeByName("lower_arm"));
-  modelNode->SetAndObserveTransformNodeID(tnode->GetID());
 
-  // Get the matrix and update it based on the forward kinematics
-  vtkMatrix4x4 *matrix = vtkMatrix4x4::SafeDownCast(tnode->GetMatrixTransformToParent());
-  for (int i = 0; i < 4; i++) {
-    for (int j=0; j <4; j ++){
-      matrix->SetElement(i,j, cartpos.operator()(i,j));
+  const char *link_names[4] = { "base", "torso", "upper_arm", "lower_arm" };
+  double link_translation_x[4] = {0, 0, 0.0075, 0};
+  double link_translation_y[4] = {-0.02, 0, 0, 0};
+  double link_translation_z[4] = { 0, 0.036, 0, 0};
+  double link_rotation_x[4] = {0,-90, 0, 90};
+  double link_rotation_y[4] = {0,0, 0, 0};
+  double link_rotation_z[4] = {0, 0, 0, 0};
+
+  // Solve the forward kinematics of the KDL tree
+  for (int k = 0; k < 3; k++){
+    KDL::Chain kdl_chain;
+    std::string base_frame(link_names[k]); // Specify the base to tip you want ie. joint 1 to 2 (base to torso)
+    std::string tip_frame(link_names[k + 1]);
+    if (!my_tree.getChain(base_frame, tip_frame, kdl_chain))
+    {
+      std::cerr << "not working" << std::endl;
+      return;
     }
+
+    // TODO: replace the hard coded link names with something like this
+    KDL::Joint kdl_joint = kdl_chain.getSegment(0).getJoint();
+    KDL::Segment kdl_segment = kdl_chain.getSegment(0);
+    std::string segmentName(kdl_segment.getName());
+    std::cerr << segmentName << std::endl;
+
+    ChainFkSolverPos_recursive fksolver = ChainFkSolverPos_recursive(kdl_chain);
+
+    // Create joint array
+    unsigned int nj = kdl_chain.getNrOfJoints();
+    KDL::JntArray jointpositions = JntArray(nj);
+
+    // Create the frame that will contain the results
+    KDL::Frame cartpos;
+    float x = 0;
+
+    // Calculate forward position kinematics
+    bool kinematics_status;
+    kinematics_status = fksolver.JntToCart(jointpositions,cartpos);
+    if(kinematics_status>=0){
+        std::cout << cartpos <<std::endl;
+        x = cartpos.operator()(2,3);
+        std::cout << cartpos.operator()(2,3) <<std::endl;
+        std::cout << "Thanks KDL!" <<std::endl;
+    }else{
+        printf("%s \n","Error: could not calculate forward kinematics :(");
+    }
+
+    // Create a transform node to add the model to so we can move it around
+    vtkNew<vtkMRMLTransformStorageNode> storageNode;
+    vtkSmartPointer<vtkMRMLTransformNode> tnode;
+
+  	storageNode->SetScene(this->GetMRMLScene());
+
+   	vtkNew<vtkMRMLTransformNode> generalTransform;
+   	generalTransform->SetScene(this->GetMRMLScene());
+    tnode = vtkSmartPointer<vtkMRMLTransformNode>::Take(vtkMRMLLinearTransformNode::New());
+   	storageNode->ReadData(tnode.GetPointer());
+    tnode->SetName("ForwardKinematics");
+    this->GetMRMLScene()->AddNode(storageNode.GetPointer());
+   	this->GetMRMLScene()->AddNode(tnode);
+    tnode->SetAndObserveStorageNodeID(storageNode->GetID());
+
+    // Create a second transform for position (translation and rotation)
+
+    vtkNew<vtkMRMLTransformStorageNode> positionStorageNode;
+    vtkSmartPointer<vtkMRMLTransformNode> ptnode;
+
+  	positionStorageNode->SetScene(this->GetMRMLScene());
+    vtkNew<vtkMRMLTransformNode> positionTransform;
+   	positionTransform->SetScene(this->GetMRMLScene());
+    ptnode = vtkSmartPointer<vtkMRMLTransformNode>::Take(vtkMRMLLinearTransformNode::New());
+   	positionStorageNode->ReadData(ptnode.GetPointer());
+    ptnode->SetName("InitialPosition");
+    this->GetMRMLScene()->AddNode(positionStorageNode.GetPointer());
+   	this->GetMRMLScene()->AddNode(ptnode);
+    ptnode->SetAndObserveStorageNodeID(positionStorageNode->GetID());
+
+    vtkTransform *modifiedTransform = vtkTransform::SafeDownCast(ptnode->GetTransformToParent());
+    modifiedTransform->Translate(link_translation_x[k + 1], link_translation_y[k + 1], link_translation_z[k + 1]);
+    modifiedTransform->RotateX(link_rotation_x[k + 1]);
+    modifiedTransform->RotateY(link_rotation_y[k + 1]);
+    modifiedTransform->RotateZ(link_rotation_z[k + 1]);
+    ptnode->SetAndObserveTransformToParent(modifiedTransform);
+    ptnode->Modified();
+
+    //Stack the transforms
+    tnode->SetAndObserveTransformNodeID(ptnode->GetID());
+
+    // Add model for the joint to transform hiearchy - so it moves with the transform
+    vtkMRMLModelNode *modelNode = vtkMRMLModelNode::SafeDownCast(this->GetMRMLScene()->GetFirstNodeByName(link_names[k + 1]));
+    modelNode->SetAndObserveTransformNodeID(tnode->GetID());
+
+    // Get the matrix and update it based on the forward kinematics
+    vtkMatrix4x4 *matrix = vtkMatrix4x4::SafeDownCast(tnode->GetMatrixTransformToParent());
+    for (int i = 0; i < 4; i++) {
+      for (int j=0; j <4; j ++){
+        matrix->SetElement(i,j, cartpos.operator()(i,j));
+      }
+    }
+    // Update the matrix for the transform
+    tnode->SetMatrixTransformToParent(matrix);
   }
-  // Update the matrix for the transform
-  tnode->SetMatrixTransformToParent(matrix);
 }
