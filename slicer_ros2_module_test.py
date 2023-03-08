e8db29652aa9278e9907c62746f4c0b8c56e2424
Aravind S Kumar <aravindskumar@aravinds-air.acasmart.jh.edu>
2023-03-08 12:33:15 -0500

    Unit tests for parameter node deletion testing

--- slicer_ros2_module_test.py
+++ slicer_ros2_module_test.py
@@ -14,6 +14,26 @@ ENVIRONMENT_CORRECTION = "export PYTHONPATH=/opt/ros/galactic/lib/python3.8/site
 ros2Module = slicer.modules.ros2
 logic = ros2Module.logic()
 
+def run_ros2_cli_command_blocking(command):
+    ros2_process = subprocess.Popen(
+        ENVIRONMENT_CORRECTION + "python3.8 /opt/ros/galactic/bin/ros2 " + command,
+        shell=True,
+        preexec_fn=os.setsid,
+    )
+    ros2_process.wait()
+    return ros2_process
+
+def run_ros2_cli_command_non_blocking(command):
+    ros2_process = subprocess.Popen(
+        ENVIRONMENT_CORRECTION + "python3.8 /opt/ros/galactic/bin/ros2 " + command,
+        shell=True,
+        preexec_fn=os.setsid,
+    )
+    return ros2_process
+
+def kill_subprocess(proc):
+    os.killpg(os.getpgid(proc.pid), subprocess.signal.SIGINT)
+
 def check_ros2_node_running(nodeName):
     # Check if the turtlesim node is running by checking the rosnode list
     nodes = (
@@ -36,24 +56,19 @@ def spin_some():
 # It creates a turtlesim node, checks if it's running, and then kills it
 class TestTurtlesimNode(unittest.TestCase):
     def setUp(self):
-        self.create_turtlesim_node_process = subprocess.Popen(
-            ENVIRONMENT_CORRECTION
-            + "python3.8 /opt/ros/galactic/bin/ros2 run turtlesim turtlesim_node",
-            shell=True,
-            preexec_fn=os.setsid,
-        )
+        self.create_turtlesim_node_process = run_ros2_cli_command_non_blocking("run turtlesim turtlesim_node")        
 
     def test_turtlesim_node_create_and_destroy(self):
         print("\nTesting creation and destruction of turtlesim node - Starting..")
         # Check if the turtlesim node is running by checking the rosnode list
-        self.assertTrue(check_ros2_node_running("/turtlesim"))
+        self.assertTrue(check_ros2_node_running("/turtlesim"), "Turtlesim node not running")
+        print("Testing creation and destruction of turtlesim node - Done")
 
     def tearDown(self):
-        os.killpg(
-            os.getpgid(self.create_turtlesim_node_process.pid), subprocess.signal.SIGINT
-        )
-        self.assertFalse(check_ros2_node_running("/turtlesim"))
-        print("Testing creation and destruction of turtlesim node - Done")
+        # Kill the turtlesim node
+        kill_subprocess(self.create_turtlesim_node_process)
+        self.assertFalse(check_ros2_node_running("/turtlesim"), "Turtlesim node still running")
+        
 
 
 # It creates a ROS2 node, adds a publisher and subscriber to it, and publishes a message
@@ -81,12 +96,12 @@ class TestCreateAndAddPubSub(unittest.TestCase):
         finalSubMessageCount = testSub.GetNumberOfMessages()
         receivedMessage = testSub.GetLastMessageYAML() # TODO: GetLastMessageString()
 
-        self.assertTrue(finalSubMessageCount - initSubMessageCount == 1)
+        self.assertTrue(finalSubMessageCount - initSubMessageCount == 1, "Message not received")
         # assert that the recceived message contains the string message - since YAML
-        self.assertTrue(messageString in receivedMessage)
+        self.assertTrue(messageString in receivedMessage, "Message not received correctly")
 
-        self.assertTrue(self.ros2Node.RemovePublisherNode("test_string_xkcd"))
-        self.assertTrue(self.ros2Node.RemoveSubscriberNode("test_string_xkcd"))
+        self.assertTrue(self.ros2Node.RemovePublisherNode("test_string_xkcd"), "Publisher not removed")
+        self.assertTrue(self.ros2Node.RemoveSubscriberNode("test_string_xkcd"), "Subscriber not removed")
         print("Testing creation and working of publisher and subscriber - Done")
 
     def test_pub_sub_deletion(self):
@@ -99,22 +114,22 @@ class TestCreateAndAddPubSub(unittest.TestCase):
         )
 
         # delete publisher which exists
-        self.assertTrue(self.ros2Node.RemovePublisherNode("test_string_xkcd"))
+        self.assertTrue(self.ros2Node.RemovePublisherNode("test_string_xkcd"), "Publisher which exists not removed")
         spin_some()
         # delete publisher which used to exist
-        self.assertFalse(self.ros2Node.RemovePublisherNode("test_string_xkcd"))
+        self.assertFalse(self.ros2Node.RemovePublisherNode("test_string_xkcd"), "Publisher which used to exist removed")
         spin_some()
         # delete publisher which never existed
-        self.assertFalse(self.ros2Node.RemovePublisherNode("random_name"))
+        self.assertFalse(self.ros2Node.RemovePublisherNode("random_name"), "Publisher which never existed removed")
         spin_some()
         # delete subscriber which exists
-        self.assertTrue(self.ros2Node.RemoveSubscriberNode("test_string_xkcd"))
+        self.assertTrue(self.ros2Node.RemoveSubscriberNode("test_string_xkcd"), "Subscriber which exists not removed")
         spin_some()
         # delete subscriber which used to exist
-        self.assertFalse(self.ros2Node.RemoveSubscriberNode("test_string_xkcd"))
+        self.assertFalse(self.ros2Node.RemoveSubscriberNode("test_string_xkcd"), "Subscriber which used to exist removed")
         spin_some()
         # delete subscriber which never existed
-        self.assertFalse(self.ros2Node.RemoveSubscriberNode("random_name"))
+        self.assertFalse(self.ros2Node.RemoveSubscriberNode("random_name"), "Subscriber which never existed removed")
         spin_some()
         print("Testing deletion of publisher and subscriber - Done")
 
@@ -127,14 +142,9 @@ class TestParameterNode(unittest.TestCase):
         print("\nCreating ROS2 node..")
         self.ros2Node = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLROS2NodeNode")
         self.ros2Node.Create("testNode")
-        self.create_turtlesim_node_process = subprocess.Popen(
-            ENVIRONMENT_CORRECTION
-            + "python3.8 /opt/ros/galactic/bin/ros2 run turtlesim turtlesim_node",
-            shell=True,
-            preexec_fn=os.setsid,
-        )
+        self.create_turtlesim_node_process = run_ros2_cli_command_non_blocking("run turtlesim turtlesim_node")        
         spin_some()
-        self.assertTrue(check_ros2_node_running("/turtlesim"))
+        self.assertTrue(check_ros2_node_running("/turtlesim"), "Turtlesim node not running")
         spin_some()
 
     def test_parameter_monitoring(self):
@@ -143,16 +153,16 @@ class TestParameterNode(unittest.TestCase):
         spin_some()
 
         # Valid Parameter which will be added
-        self.assertEqual(testParam.GetParameterType("background_r"), "")
-        self.assertEqual(testParam.GetParameterType("background_r"), "")
+        self.assertEqual(testParam.GetParameterType("background_r"), "", "Parameter type not empty")
+        self.assertEqual(testParam.GetParameterType("background_r"), "", "Parameter type not empty")
 
         # Parameter which does not exist
-        self.assertEqual(testParam.GetParameterType("background_y"), "")
-        self.assertEqual(testParam.GetParameterType("background_y"), "")
+        self.assertEqual(testParam.GetParameterType("background_y"), "", "Parameter type not empty")
+        self.assertEqual(testParam.GetParameterType("background_y"), "", "Parameter type not empty")
 
         # Parameter which exists but would not be added to monitor
-        self.assertEqual(testParam.GetParameterType("background_g"), "")
-        self.assertEqual(testParam.GetParameterType("background_g"), "")
+        self.assertEqual(testParam.GetParameterType("background_g"), "", "Parameter type not empty")
+        self.assertEqual(testParam.GetParameterType("background_g"), "", "Parameter type not empty")
 
         testParam.AddParameter("background_r")
         testParam.AddParameter("background_y")
@@ -160,17 +170,14 @@ class TestParameterNode(unittest.TestCase):
         while(testParam.IsParameterSet("background_r") == False):
             spin_some()
 
-        self.assertEqual(testParam.GetParameterType("background_r"), "integer")
+        self.assertEqual(testParam.GetParameterType("background_r"), "integer", "Parameter type not integer")
 
-        self.assertFalse(testParam.IsParameterSet("background_y", True))
-        self.assertEqual(testParam.GetParameterType("background_y"), "")
+        self.assertFalse(testParam.IsParameterSet("background_y", True), "Parameter type not empty")
+        self.assertEqual(testParam.GetParameterType("background_y"), "", "Parameter type not empty")
 
         # Update parameter value
-        change_param_value = subprocess.Popen(
-            ENVIRONMENT_CORRECTION
-            + "python3.8 /opt/ros/galactic/bin/ros2 param set /turtlesim background_g 150",
-            shell=True,
-            preexec_fn=os.setsid,
+        change_param_value = run_ros2_cli_command_blocking(
+            "param set /turtlesim background_g 150"
         )
 
         while(testParam.IsParameterSet("background_g", True) == False):
@@ -182,12 +189,27 @@ class TestParameterNode(unittest.TestCase):
 
         # delete parameter node
         self.assertTrue(self.ros2Node.RemoveParameterNode("/turtlesim"))
+        print("Testing creation and working of parameter node - Done")
+
+    def test_parameter_deletion(self):
+        print("\nTesting deletion of parameter node - Starting..")
+        testParam = self.ros2Node.CreateAndAddParameter("/turtlesim")
+        spin_some()
+
+        # delete parameter node which exists
+        self.assertTrue(self.ros2Node.RemoveParameterNode("/turtlesim"), "Failed to delete parameter node which exists")
+        spin_some()
+        # delete parameter node which used to exist
+        self.assertFalse(self.ros2Node.RemoveParameterNode("/turtlesim"), "Deleted parameter node which used to exist")
+        spin_some()
+        # delete parameter node which never existed
+        self.assertFalse(self.ros2Node.RemoveParameterNode("/random_name"), "Deleted parameter node which never existed")
+        spin_some()
+        print("Testing deletion of parameter node - Done")
 
     def tearDown(self):
         # self.ros2Node.Destroy()
-        os.killpg(
-            os.getpgid(self.create_turtlesim_node_process.pid), subprocess.signal.SIGINT
-        )
+        kill_subprocess(self.create_turtlesim_node_process)
 
 class TestBroadcasterNode(unittest.TestCase):
     def setUp(self):
