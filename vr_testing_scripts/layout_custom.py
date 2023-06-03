14dbd7d25836c5f10da29ba836fc2197b8d0e65b
aravind <aravindskumar1998@gmail.com>
2023-06-02 21:54:51 -0400

    cleared ununsed functions

--- vr_testing_scripts/layout_custom.py
+++ vr_testing_scripts/layout_custom.py
@@ -9,108 +9,48 @@ except:
 
 class StereoView:
     def __init__(self):
-        self.layout_manager = slicer.app.layoutManager()
-        self.threeDWidget1 = self.layout_manager.threeDWidget(0)
-        self.threeDWidget2 = self.layout_manager.threeDWidget(1)
+        self.layoutManager = slicer.app.layoutManager()
+        self.threeDWidget1 = self.layoutManager.threeDWidget(0)
+        self.threeDWidget2 = self.layoutManager.threeDWidget(1)
         # Get the view nodes from the 3D widgets
         self.viewNode1 = self.threeDWidget1.mrmlViewNode()
         self.viewNode2 = self.threeDWidget2.mrmlViewNode()
         # Get the active camera nodes for both views
-        self.camera_node1 = slicer.modules.cameras.logic(
-        ).GetViewActiveCameraNode(self.viewNode1)
-        self.camera_node2 = slicer.modules.cameras.logic(
-        ).GetViewActiveCameraNode(self.viewNode2)
+        self.cameraNode1 = slicer.modules.cameras.logic(
+            ).GetViewActiveCameraNode(self.viewNode1)
+        self.cameraNode2 = slicer.modules.cameras.logic(
+            ).GetViewActiveCameraNode(self.viewNode2)
 
-        self.transformation_to_center = vtk.vtkMatrix4x4()
+    def displaceCamera(self, displacementRotationMatrix4x4, positionDisplacementVector):
+        leftCameraCurrentTransform, focal_disp_magnitude = self.GetCameraTransform(self.cameraNode1)
 
-    def set_separation(self, x, y, z, reset=True):  # Unrefined
+        leftCameraNextTransform = vtk.vtkMatrix4x4()
 
-        self.center_to_left = vtk.vtkMatrix4x4()
-        self.center_to_right = vtk.vtkMatrix4x4()
-
-        self.center_to_left.SetElement(0, 3, -x / 2)
-        self.center_to_right.SetElement(0, 3, x / 2)
-
-        self.center_to_left.SetElement(1, 3, -y / 2)
-        self.center_to_right.SetElement(1, 3, y / 2)
-
-        self.center_to_left.SetElement(2, 3, -z / 2)
-        self.center_to_right.SetElement(2, 3, z / 2)
-
-
-    def get_transformation_to_camera(self, camera_node):
-        position = camera_node.GetPosition()
-        focal_point = camera_node.GetFocalPoint()
-        view_up = camera_node.GetViewUp()
-
-        z_axis = view_up
-        z_axis /= np.linalg.norm(z_axis)  # Normalize
+        currentCameraPosition = [leftCameraCurrentTransform.GetElement(
+            0, 3), leftCameraCurrentTransform.GetElement(1, 3), leftCameraCurrentTransform.GetElement(2, 3)]
         
-        focal_displacement = np.array(focal_point) - np.array(position)
-        magnitude = np.linalg.norm(focal_displacement)
-
-        y_axis = focal_displacement
-        y_axis /= np.linalg.norm(y_axis)  # Normalize
-
-        x_axis = np.cross(y_axis, z_axis)
-        x_axis /= np.linalg.norm(x_axis)  # Normalize
-
-        transformation_to_camera = vtk.vtkMatrix4x4()
-        transformation_to_camera.Identity()
-        for i in range(3):
-            transformation_to_camera.SetElement(i, 0, x_axis[i])
-            transformation_to_camera.SetElement(i, 1, y_axis[i])
-            transformation_to_camera.SetElement(i, 2, z_axis[i])
-            transformation_to_camera.SetElement(i, 3, position[i])
-
-        return transformation_to_camera, magnitude
-    
-    def set_camera_node_settings_from_transform(self, camera_node, transform, magnitude):
-
-        position = [transform.GetElement(0, 3), transform.GetElement(1, 3), transform.GetElement(2, 3)]
-        
-        y_axis = [transform.GetElement(0, 1), transform.GetElement(1, 1), transform.GetElement(2, 1)]
-        focal_displacement = np.array(y_axis) * magnitude
-        focal_point = np.array(position) + focal_displacement
-
-        view_up = [transform.GetElement(0, 2), transform.GetElement(1, 2), transform.GetElement(2, 2)]
-
-        camera_node.SetPosition(position)
-        camera_node.SetFocalPoint(focal_point)
-        camera_node.SetViewUp(view_up)
-
-    def displace_camera(self, displacement_transform, translation_vector):
-        initial_transform, focal_disp_magnitude = self.get_transformation_to_camera(self.camera_node1)
-
-        new_center_transform_matrix = vtk.vtkMatrix4x4()
-
-        initial_center_position = [initial_transform.GetElement(
-            0, 3), initial_transform.GetElement(1, 3), initial_transform.GetElement(2, 3)]
-        
-        initial_transform.SetElement(0, 3, 0)
-        initial_transform.SetElement(1, 3, 0)
-        initial_transform.SetElement(2, 3, 0)
+        leftCameraCurrentTransform.SetElement(0, 3, 0)
+        leftCameraCurrentTransform.SetElement(1, 3, 0)
+        leftCameraCurrentTransform.SetElement(2, 3, 0)
 
         vtk.vtkMatrix4x4.Multiply4x4(
-            displacement_transform, initial_transform, new_center_transform_matrix)
+            displacementRotationMatrix4x4, leftCameraCurrentTransform, leftCameraNextTransform)
         
-        new_center_transform_matrix.SetElement(
-            0, 3,  translation_vector[0] + initial_center_position[0])
-        new_center_transform_matrix.SetElement(
-            1, 3,  translation_vector[1] + initial_center_position[1])
-        new_center_transform_matrix.SetElement(
-            2, 3,  translation_vector[2] + initial_center_position[2])
+        leftCameraNextTransform.SetElement(
+            0, 3,  positionDisplacementVector[0] + currentCameraPosition[0])
+        leftCameraNextTransform.SetElement(
+            1, 3,  positionDisplacementVector[1] + currentCameraPosition[1])
+        leftCameraNextTransform.SetElement(
+            2, 3,  positionDisplacementVector[2] + currentCameraPosition[2])
         
-        self.transformation_to_center = new_center_transform_matrix
+        self.SetCameraTransform(self.cameraNode1, leftCameraNextTransform, focal_disp_magnitude)
+        self.SetCameraTransform(self.cameraNode2, leftCameraNextTransform, focal_disp_magnitude)
 
-        self.set_camera_node_settings_from_transform(self.camera_node1, new_center_transform_matrix, focal_disp_magnitude)
-        self.set_camera_node_settings_from_transform(self.camera_node2, new_center_transform_matrix, focal_disp_magnitude)
+        self.leftCameraSittingTransform.SetMatrixTransformToParent(
+            leftCameraNextTransform)
 
-        self.left_camera_transform.SetMatrixTransformToParent(
-            self.transformation_to_center)
-
-        self.right_camera_transform.SetMatrixTransformToParent(
-            self.transformation_to_center)
+        self.rightCameraSittingTransform.SetMatrixTransformToParent(
+            leftCameraNextTransform)
 
     def setup(self):
         ros2Node = slicer.mrmlScene.GetFirstNodeByName('ros2:node:slicer')
@@ -122,68 +62,93 @@ class StereoView:
         self.buttonSubscriber = ros2Node.CreateAndAddSubscriberNode(
             "vtkMRMLROS2SubscriberJoyNode", "/console/camera")
         
-        if not slicer.mrmlScene.GetFirstNodeByName("left_camera_transform"):
-            self.left_camera_transform = slicer.vtkMRMLLinearTransformNode()
-            self.left_camera_transform.SetName("left_camera_transform")
-            slicer.mrmlScene.AddNode(self.left_camera_transform)
+        if not slicer.mrmlScene.GetFirstNodeByName("leftCameraSittingTransform"):
+            self.leftCameraSittingTransform = slicer.vtkMRMLLinearTransformNode()
+            self.leftCameraSittingTransform.SetName("leftCameraSittingTransform")
+            slicer.mrmlScene.AddNode(self.leftCameraSittingTransform)
 
-        if not slicer.mrmlScene.GetFirstNodeByName("right_camera_transform"):
-            self.right_camera_transform = slicer.vtkMRMLLinearTransformNode()
-            self.right_camera_transform.SetName("right_camera_transform")
-            slicer.mrmlScene.AddNode(self.right_camera_transform)
+        if not slicer.mrmlScene.GetFirstNodeByName("rightCameraSittingTransform"):
+            self.rightCameraSittingTransform = slicer.vtkMRMLLinearTransformNode()
+            self.rightCameraSittingTransform.SetName("rightCameraSittingTransform")
+            slicer.mrmlScene.AddNode(self.rightCameraSittingTransform)
 
-        displacement = vtk.vtkMatrix4x4()
-
-        self.move_robot = False
-
-        translate = [0, 0, 0]
-
-        self.displace_camera(displacement, translate)
+        self.moveCamera = False
 
         self.buttonObserverID = self.buttonSubscriber.AddObserver(
             "ModifiedEvent", self._buttonCallback)
         observerId = self.lookupNode.AddObserver(
             slicer.vtkMRMLTransformNode.TransformModifiedEvent, self._callback)
+        
+    def GetCameraTransform(cameraNode):
+        position = cameraNode.GetPosition()
+        focalPoint = cameraNode.GetFocalPoint()
+        viewUp = cameraNode.GetViewUp()
+
+        focalDisplacement = np.array(focalPoint) - np.array(position)
+        magnitude = np.linalg.norm(focalDisplacement)
+
+        zz = viewUp
+        zz /= np.linalg.norm(zz)  # Normalize
+
+        yy = focalDisplacement
+        yy /= np.linalg.norm(yy)  # Normalize
+
+        xx = np.cross(yy, zz)
+        xx /= np.linalg.norm(xx)  # Normalize
+
+        cameraTransform = vtk.vtkMatrix4x4()
+        cameraTransform.Identity()
+        for i in range(3):
+            cameraTransform.SetElement(i, 0, xx[i])
+            cameraTransform.SetElement(i, 1, yy[i])
+            cameraTransform.SetElement(i, 2, zz[i])
+            cameraTransform.SetElement(i, 3, position[i])
+
+        return cameraTransform, magnitude
+    
+    def SetCameraTransform(cameraNode, transform, magnitude):
+
+        position = [transform.GetElement(0, 3), transform.GetElement(1, 3), transform.GetElement(2, 3)]
+        
+        yy = [transform.GetElement(0, 1), transform.GetElement(1, 1), transform.GetElement(2, 1)]
+        focalDisplacement = np.array(yy) * magnitude
+        focalPoint = np.array(position) + focalDisplacement
+
+        viewUp = [transform.GetElement(0, 2), transform.GetElement(1, 2), transform.GetElement(2, 2)]
+
+        cameraNode.SetPosition(position)
+        cameraNode.SetFocalPoint(focalPoint)
+        cameraNode.SetViewUp(viewUp)
 
     def _buttonCallback(self, caller, event):
         msg_yaml = caller.GetLastMessageYAML()
         msg = yaml.load(msg_yaml, Loader=yaml.FullLoader)
         button = msg['buttons'][0]
 
-        if button == 1 and self.move_robot == False:
-            self.start_motion()
-        elif button == 0 and self.move_robot == True:
-            self.stop_motion()
+        if button == 1 and self.moveCamera == False:
+            self.startCameraControl()
+        elif button == 0 and self.moveCamera == True:
+            self.stopCameraControl()
         else:
             print(f"Received button value: {button}")
 
-    def start_motion(self):
-        self.initial_robot_transform = vtk.vtkMatrix4x4()
-        self.initial_robot_transform.DeepCopy(
+    def startCameraControl(self):
+        self.currentControllerTransform = vtk.vtkMatrix4x4()
+        self.currentControllerTransform.DeepCopy(
             self.lookupNode.GetMatrixTransformToParent())
-        self.move_robot = True
+        self.moveCamera = True
 
-    def stop_motion(self):
-        self.move_robot = False
+    def stopCameraControl(self):
+        self.moveCamera = False
 
     def _callback(self, caller, event):
-        if self.move_robot:
-            self.lastTransform = caller.GetMatrixTransformToParent()
-            displacement, translation_vector = findDisplacementTransform(
-                self.initial_robot_transform, self.lastTransform, self.scale_factor)
-            self.displace_camera(displacement, translation_vector)
-            # copy contents of lastTransform into initial_robot_transform they are vtkMatrix4x4 objects
-            self.initial_robot_transform.DeepCopy(self.lastTransform)
-
-    def calibrate(self):
-        self.camera_node1.SetViewUp([0, 0, 1])
-        self.camera_node2.SetViewUp([0, 0, 1])
-
-        # self.camera_node1.SetFocalPoint([0, 0, 0])
-        # self.camera_node2.SetFocalPoint([0, 0, 0])
-
-        # self.camera_node1.SetPosition([0, 0, 0])
-        # self.camera_node2.SetPosition([0, 0, 0])
+        if self.moveCamera:
+            self.nextControllerTransform = caller.GetMatrixTransformToParent()
+            displacement, positionDisplacementVector = findDisplacementTransform(
+                self.currentControllerTransform, self.nextControllerTransform, self.scale_factor)
+            self.displaceCamera(displacement, positionDisplacementVector)
+            # copy contents of nextControllerTransform into currentControllerTransform they are vtkMatrix4x4 objects
+            self.currentControllerTransform.DeepCopy(self.nextControllerTransform)
 
 
 def add_model_to_scene(path_to_model):
@@ -193,7 +158,7 @@ def add_model_to_scene(path_to_model):
         return
 
 
-def create_custom_layout(position=[100, 100], resolution=[1920, 1080]):
+def createCustomLayout(position=[100, 100], resolution=[1920, 1080]):
     customLayout = """
     <layout type="vertical" >
         <item>
@@ -244,7 +209,7 @@ def findDisplacementTransform(startTransform, endTransform, scale_factor):
 
     scale_factor = 0.2
 
-    translation_vector = [(endTransform.GetElement(
+    positionDisplacementVector = [(endTransform.GetElement(
         i, 3) - startTransform.GetElement(i, 3)) * scale_factor for i in range(3)]
 
     # divide position by scale factor
@@ -255,38 +220,17 @@ def findDisplacementTransform(startTransform, endTransform, scale_factor):
     displacementTransform.SetElement(
         2, 3, 0)
 
-    return displacementTransform, translation_vector
-
-def visualize_transform(transform_name):
-    # Get the transform node
-    transformNode = slicer.mrmlScene.GetFirstNodeByName(transform_name)
-
-    # Get the associated display node
-    displayNode = transformNode.GetDisplayNode()
-
-    # If there is no display node associated with the transform, create one
-    if displayNode is None:
-        displayNode = slicer.vtkMRMLTransformDisplayNode()
-        slicer.mrmlScene.AddNode(displayNode)
-        transformNode.SetAndObserveDisplayNodeID(displayNode.GetID())
-
-    # If visibility is on, turn it off. If it is off, turn it on.
-    visibility = displayNode.GetVisibility()
-    displayNode.SetVisibility(not visibility)
+    return displacementTransform, positionDisplacementVector
 
 if __name__ == "__main__":
 
     # Create a custom layout
-    popw = create_custom_layout(resolution=[1280, 720])
+    popw = createCustomLayout(resolution=[1280, 720])
     popw.show()  # only works if called in the main function
     stereo = StereoView()
     # add_model_to_scene("TumorModel.vtk")
-    # stereo.calibrate()
-    # add_model_to_scene("base.stl")
-    stereo.set_separation(0, 0, 0)
-
     stereo.setup()
-    # stereo.start_motion()
+
 
 
 # exec(open('layout_custom.py').read())
